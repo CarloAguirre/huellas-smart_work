60b14629c60b28e9f4d01de810529619db182cd4
Carlo Aguirre <carlo_aguirre@outlook.cl>
2024-06-19 16:40:55 -0400

    date filter advance

--- src/app/resultados/dashboard/dashboard.component.ts
+++ src/app/resultados/dashboard/dashboard.component.ts
@@ -51,52 +51,19 @@ export class DashboardComponent implements OnInit, OnChanges {
   }
 
   ngOnInit(): void {
-    this.updateChart()
+
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.calculateSortedCategories();
-    this.updateChart()
+    this.totalCategorias = this.totalCategorias
+    if(changes['alcanceUno'] || changes['alcanceDos'] || changes['alcanceTres']){
+
+    }
+    // this.updateChart()
   }
 
-  updateChart() {
-    const totalAlcance1 = this.resumenEmisiones.reduce((acc, curr) => acc + curr.totalAlcance1, 0);
-    const totalAlcance2 = this.resumenEmisiones.reduce((acc, curr) => acc + curr.totalAlcance2, 0);
-    const totalAlcance3 = this.resumenEmisiones.reduce((acc, curr) => acc + curr.totalAlcance3, 0);
 
-    const total = totalAlcance1 + totalAlcance2 + totalAlcance3;
-
-    this.alcanceUno = totalAlcance1 > 0 ? ((totalAlcance1 / total) * 100).toFixed(2) : 0;
-    this.alcanceDos = totalAlcance2 > 0 ? ((totalAlcance2 / total) * 100).toFixed(2) : 0;
-    this.alcanceTres = totalAlcance3 > 0 ? ((totalAlcance3 / total) * 100).toFixed(2) : 0;
-
-
-    console.log(this.alcanceUno)
-    console.log(this.alcanceDos)
-    console.log(this.alcanceTres)
-    this.chartOptions.series = [
-      {
-        name: "Alcance 1",
-        data: [parseFloat(this.alcanceUno)]
-      },
-      {
-        name: "Alcance 2",
-        data: [parseFloat(this.alcanceDos)]
-      },
-      {
-        name: "Alcance 3",
-        data: [parseFloat(this.alcanceTres)]
-      }
-
-    ];
-    this.chartOptions.xaxis.categories = ["Porcentaje"];
-
-    this.chartOptions.title.text = "Porcentaje de Emisiones por Alcance";
-
-    this.chartOptions.tooltip.y.formatter = function(val: any) {
-      return val.toFixed(2) + "%";
-    };
-  }
 
   calculateSortedCategories(): void {
 
--- src/app/resultados/resultados.component.ts
+++ src/app/resultados/resultados.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit, SimpleChanges, ViewChild  } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { DataStore } from 'aws-amplify';
-import { Emision } from 'src/models'; // La ruta puede variar según donde se generó tu modelo.
+import { Emision } from 'src/models';
 import { SelectionModel } from '@angular/cdk/collections';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import {EmisionesResumen} from './dashboard/interfaces/EmisionesResumen.interfaces'
+import { EmisionesResumen } from './dashboard/interfaces/EmisionesResumen.interfaces';
 import {
   ApexAxisChartSeries,
   ApexChart,
-  ChartComponent,
   ApexDataLabels,
   ApexXAxis,
   ApexPlotOptions,
@@ -22,7 +21,7 @@ import { TotalCategorias, Emisiones } from './dashboard/interfaces/TotalCategori
 import { CategoryMap } from './dashboard/interfaces/CategoryMap.interfaces';
 import { categoryMap } from './dashboard/data/category-map';
 import { totalCategorias } from './dashboard/data/total-caregoria';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 export type ChartOptions = {
   series: ApexAxisChartSeries | any;
@@ -47,25 +46,26 @@ export type ChartOptions = {
 export class ResultadosComponent implements OnInit {
   public chartOptions: Partial<ChartOptions>;
   public emisiones: Emision[] = [];
-  public resumenEmisiones: EmisionesResumen[] = []; // Añadir esta línea para almacenar el resumen
-  public alcanceUno: any = null  // %
-  public alcanceDos: any = null  // %
-  public alcanceTres: any = null  // %
-  public totalAlcanceUno: any | null = null
-  public totalAlcanceDos: any | null = null
-  public totalAlcanceTres: any | null = null
-  public totalAlcance: any | null = null
-  public categoryMap: CategoryMap = categoryMap
+  public resumenEmisiones: EmisionesResumen[] = [];
+  public alcanceUno: any = null;
+  public alcanceDos: any = null;
+  public alcanceTres: any = null;
+  public totalAlcanceUno: any | null = null;
+  public totalAlcanceDos: any | null = null;
+  public totalAlcanceTres: any | null = null;
+  public totalAlcance: any | null = null;
+  public categoryMap: CategoryMap = categoryMap;
   public scopeMap: { [key: string]: keyof TotalCategorias } = {
     "Alcance 1": "alcanceUno",
     "Alcance 2": "alcanceDos",
     "Alcance 3": "alcanceTres"
   };
   public totalCategorias!: TotalCategorias;
+  dateRangeForm: FormGroup;
 
   @ViewChild(DashboardComponent) dashboardComponent!: DashboardComponent;
 
-  constructor() {
+  constructor(private changeDetector: ChangeDetectorRef, private fb: FormBuilder) {
     this.chartOptions = {
       series: [
         {
@@ -103,7 +103,6 @@ export class ResultadosComponent implements OnInit {
       xaxis: {
         categories: [' ']
       },
-
       tooltip: {
         y: {
           formatter: function(val: any) {
@@ -121,8 +120,11 @@ export class ResultadosComponent implements OnInit {
       }
     };
 
-    this.totalCategorias = totalCategorias;
-    // this.updateChart()
+    this.dateRangeForm = this.fb.group({
+      start: [null],
+      end: [null]
+    });
+    this.totalCategorias = JSON.parse(JSON.stringify(totalCategorias));
   }
 
   selection = new SelectionModel<Emision>(true, []);
@@ -150,129 +152,160 @@ export class ResultadosComponent implements OnInit {
     return meses[mes] || '';
   }
 
-  // calculateTotalCategorias() {
-
-  //   this.emisiones.forEach(emision => {
-  //     const categoryKey = this.categoryMap[emision.CATEGORIA];
-  //     const scopeKey = this.scopeMap[emision.ALCANCE];
-
-  //     if (categoryKey && scopeKey) {
-  //       if (this.totalCategorias[scopeKey][categoryKey] === null) {
-  //         this.totalCategorias[scopeKey][categoryKey] = 0;
-  //       }
-  //       this.totalCategorias[scopeKey][categoryKey]! += emision.CO2 + emision.CH4 + emision.HFC + emision.N2O + emision.NF3 + emision.PFC + emision.SF6;
-  //     }
-  //     console.log(this.totalCategorias)
-  //   });
-
-  // }
-
+  applyDateFilter() {
+    const { start, end } = this.dateRangeForm.value;
+    if (start && end) {
+      const filteredEmisiones = this.emisiones.filter(emision => {
+        const inicio = new Date(emision.InicioPeriodo);
+        const termino = new Date(emision.TerminoPeriodo);
+        return (inicio >= start && termino <= end);
+      });
+      this.calculateEmisionesMensuales(filteredEmisiones);
+    }
+  }
 
   async ngOnInit() {
-
     try {
       this.emisiones = await DataStore.query(Emision);
+      this.calculateEmisionesMensuales(this.emisiones);
+    } catch (error) {
+      console.error('Error al consultar los datos:', error);
+    }
+  }
 
-      const emisionesMensuales: { [key: string]: EmisionesResumen } = {};
-      this.emisiones.forEach((emision) => {
-        const fechaInicio = new Date(emision.InicioPeriodo);
-        const fechaTermino = new Date(emision.TerminoPeriodo);
-        const añoInicio = fechaInicio.getFullYear();
-        const añoTermino = fechaTermino.getFullYear();
+  calculateEmisionesMensuales(emisiones: Emision[]) {
+    const emisionesMensuales: { [key: string]: EmisionesResumen } = {};
+    this.totalCategorias = JSON.parse(JSON.stringify(totalCategorias)); // Reiniciar totalCategorias
+    this.totalAlcanceUno = 0;
+    this.totalAlcanceDos = 0;
+    this.totalAlcanceTres = 0;
 
-        let acumuladorFactor = 0; // Acumulador para los factores proporcionales
+    emisiones.forEach((emision) => {
+      const fechaInicio = new Date(emision.InicioPeriodo);
+      const fechaTermino = new Date(emision.TerminoPeriodo);
+      const añoInicio = fechaInicio.getFullYear();
+      const añoTermino = fechaTermino.getFullYear();
 
-        for (let año = añoInicio; año <= añoTermino; año++) {
-          const mesComienzo = año === añoInicio ? fechaInicio.getMonth() : 0;
-          const mesFin = año === añoTermino ? fechaTermino.getMonth() : 11;
+      let acumuladorFactor = 0; // Acumulador para los factores proporcionales
 
-          for (let mes = mesComienzo; mes <= mesFin; mes++) {
-            const inicioDelMesActual = new Date(año, mes, 1);
-            const finDelMesActual = new Date(año, mes + 1, 0);
-            const fechaInicioConsiderada = fechaInicio > inicioDelMesActual ? fechaInicio : inicioDelMesActual;
-            const fechaTerminoConsiderada = fechaTermino < finDelMesActual ? fechaTermino : finDelMesActual;
+      for (let año = añoInicio; año <= añoTermino; año++) {
+        const mesComienzo = año === añoInicio ? fechaInicio.getMonth() : 0;
+        const mesFin = año === añoTermino ? fechaTermino.getMonth() : 11;
 
-            if (fechaInicioConsiderada <= fechaTerminoConsiderada) {
-              const diasDelMesEnPeriodo = (fechaTerminoConsiderada.getTime() - fechaInicioConsiderada.getTime()) / (1000 * 3600 * 24) + 1;
-              const diasTotales = (fechaTermino.getTime() - fechaInicio.getTime()) / (1000 * 3600 * 24) + 1;
-              let factorProporcional = diasDelMesEnPeriodo / diasTotales;
+        for (let mes = mesComienzo; mes <= mesFin; mes++) {
+          const inicioDelMesActual = new Date(año, mes, 1);
+          const finDelMesActual = new Date(año, mes + 1, 0);
+          const fechaInicioConsiderada = fechaInicio > inicioDelMesActual ? fechaInicio : inicioDelMesActual;
+          const fechaTerminoConsiderada = fechaTermino < finDelMesActual ? fechaTermino : finDelMesActual;
 
-              const esUltimoMes = año === añoTermino && mes === mesFin;
-              if (esUltimoMes) {
-                // Ajustar el factor proporcional del último mes
-                factorProporcional = 1 - acumuladorFactor;
-              }
+          if (fechaInicioConsiderada <= fechaTerminoConsiderada) {
+            const diasDelMesEnPeriodo = (fechaTerminoConsiderada.getTime() - fechaInicioConsiderada.getTime()) / (1000 * 3600 * 24) + 1;
+            const diasTotales = (fechaTermino.getTime() - fechaInicio.getTime()) / (1000 * 3600 * 24) + 1;
+            let factorProporcional = diasDelMesEnPeriodo / diasTotales;
 
-              const key = `${this.mesAbreviado(mes)}-${año}`;
+            const esUltimoMes = año === añoTermino && mes === mesFin;
+            if (esUltimoMes) {
+              // Ajustar el factor proporcional del último mes
+              factorProporcional = 1 - acumuladorFactor;
+            }
 
-              if (!emisionesMensuales[key]) {
-                emisionesMensuales[key] = {
-                  periodo: `${this.mesAbreviado(mes)}-${año}`,
-                  totalTonCO2eq: 0,
-                  totalAlcance1: 0,
-                  totalAlcance2: 0,
-                  totalAlcance3: 0,
-                  totalCO2: 0,
-                  totalCH4: 0,
-                  totalN2O: 0,
-                  totalSF6: 0,
-                  totalHFC: 0,
-                  totalPFC: 0,
-                  totalNF3: 0,
-                };
-              }
+            const key = `${this.mesAbreviado(mes)}-${año}`;
 
-              // Suma basada en el alcance
-              const totalEmissionForThisEntry = (emision.CO2 + emision.CH4 + emision.N2O + emision.SF6 + emision.HFC + emision.PFC + emision.NF3) * factorProporcional;
+            if (!emisionesMensuales[key]) {
+              emisionesMensuales[key] = {
+                periodo: `${this.mesAbreviado(mes)}-${año}`,
+                totalTonCO2eq: 0,
+                totalAlcance1: 0,
+                totalAlcance2: 0,
+                totalAlcance3: 0,
+                totalCO2: 0,
+                totalCH4: 0,
+                totalN2O: 0,
+                totalSF6: 0,
+                totalHFC: 0,
+                totalPFC: 0,
+                totalNF3: 0,
+              };
+            }
 
-              if (emision.ALCANCE === "Alcance 1") {
-                emisionesMensuales[key].totalAlcance1 += totalEmissionForThisEntry;
-              } else if (emision.ALCANCE === "Alcance 2") {
-                emisionesMensuales[key].totalAlcance2 += totalEmissionForThisEntry;
-              } else if (emision.ALCANCE === "Alcance 3") {
-                emisionesMensuales[key].totalAlcance3 += totalEmissionForThisEntry;
-              }
+            // Suma basada en el alcance
+            const totalEmissionForThisEntry = (emision.CO2 + emision.CH4 + emision.N2O + emision.SF6 + emision.HFC + emision.PFC + emision.NF3) * factorProporcional;
 
-              emisionesMensuales[key].totalCO2 += emision.CO2 * factorProporcional;
-              emisionesMensuales[key].totalCH4 += emision.CH4 * factorProporcional;
-              emisionesMensuales[key].totalN2O += emision.N2O * factorProporcional;
-              emisionesMensuales[key].totalSF6 += emision.SF6 * factorProporcional;
-              emisionesMensuales[key].totalHFC += emision.HFC * factorProporcional;
-              emisionesMensuales[key].totalPFC += emision.PFC * factorProporcional;
-              emisionesMensuales[key].totalNF3 += emision.NF3 * factorProporcional;
-              emisionesMensuales[key].totalTonCO2eq += totalEmissionForThisEntry/1000; // Ya has aplicado el factor proporcional
-              if (esUltimoMes) {
-                acumuladorFactor = 0;
-              } else {
-                acumuladorFactor += factorProporcional;
-              }
+            if (emision.ALCANCE === "Alcance 1") {
+              emisionesMensuales[key].totalAlcance1 += totalEmissionForThisEntry;
+            } else if (emision.ALCANCE === "Alcance 2") {
+              emisionesMensuales[key].totalAlcance2 += totalEmissionForThisEntry;
+            } else if (emision.ALCANCE === "Alcance 3") {
+              emisionesMensuales[key].totalAlcance3 += totalEmissionForThisEntry;
+            }
+
+            emisionesMensuales[key].totalCO2 += emision.CO2 * factorProporcional;
+            emisionesMensuales[key].totalCH4 += emision.CH4 * factorProporcional;
+            emisionesMensuales[key].totalN2O += emision.N2O * factorProporcional;
+            emisionesMensuales[key].totalSF6 += emision.SF6 * factorProporcional;
+            emisionesMensuales[key].totalHFC += emision.HFC * factorProporcional;
+            emisionesMensuales[key].totalPFC += emision.PFC * factorProporcional;
+            emisionesMensuales[key].totalNF3 += emision.NF3 * factorProporcional;
+            emisionesMensuales[key].totalTonCO2eq += totalEmissionForThisEntry / 1000; // Ya has aplicado el factor proporcional
+            if (esUltimoMes) {
+              acumuladorFactor = 0;
+            } else {
+              acumuladorFactor += factorProporcional;
             }
           }
         }
+      }
 
-        this.resumenEmisiones = Object.values(emisionesMensuales);
+      const categoryKey = this.categoryMap[emision.CATEGORIA];
+      const scopeKey = this.scopeMap[emision.ALCANCE];
 
-        const categoryKey = this.categoryMap[emision.CATEGORIA];
-        const scopeKey = this.scopeMap[emision.ALCANCE];
+      if (categoryKey && scopeKey) {
+        this.totalCategorias[scopeKey][categoryKey]! += emision.CO2 + emision.CH4 + emision.HFC + emision.N2O + emision.NF3 + emision.PFC + emision.SF6;
+      }
+    });
 
-        if (categoryKey && scopeKey) {
-          this.totalCategorias[scopeKey][categoryKey]! += emision.CO2 + emision.CH4 + emision.HFC + emision.N2O + emision.NF3 + emision.PFC + emision.SF6;;
-        }
+    this.resumenEmisiones = Object.values(emisionesMensuales);
+    this.totalAlcanceUno = this.resumenEmisiones.reduce((acc, curr) => acc + curr.totalAlcance1, 0) / 1000;
+    this.totalAlcanceDos = this.resumenEmisiones.reduce((acc, curr) => acc + curr.totalAlcance2, 0) / 1000;
+    this.totalAlcanceTres = this.resumenEmisiones.reduce((acc, curr) => acc + curr.totalAlcance3, 0) / 1000;
+    this.totalAlcance = this.totalAlcanceUno + this.totalAlcanceDos + this.totalAlcanceTres;
 
-        });
-        } catch (error) {
-          console.error('Error al consultar los datos:', error);
-        }
+    this.updateChart();
+    this.changeDetector.detectChanges();
+  }
 
-        this.resumenEmisiones.forEach(periodo => {
-          this.totalAlcanceUno += periodo.totalAlcance1/1000
-          this.totalAlcanceDos += periodo.totalAlcance2/1000
-          this.totalAlcanceTres += periodo.totalAlcance3/1000
+  updateChart() {
+    const totalAlcance1 = this.resumenEmisiones.reduce((acc, curr) => acc + curr.totalAlcance1, 0);
+    const totalAlcance2 = this.resumenEmisiones.reduce((acc, curr) => acc + curr.totalAlcance2, 0);
+    const totalAlcance3 = this.resumenEmisiones.reduce((acc, curr) => acc + curr.totalAlcance3, 0);
 
-        });
-        this.totalAlcance += this.totalAlcanceUno + this.totalAlcanceDos + this.totalAlcanceTres
-    }
+    const total = totalAlcance1 + totalAlcance2 + totalAlcance3;
 
+    this.alcanceUno = totalAlcance1 > 0 ? ((totalAlcance1 / total) * 100).toFixed(2) : 0;
+    this.alcanceDos = totalAlcance2 > 0 ? ((totalAlcance2 / total) * 100).toFixed(2) : 0;
+    this.alcanceTres = totalAlcance3 > 0 ? ((totalAlcance3 / total) * 100).toFixed(2) : 0;
+
+    this.chartOptions.series = [
+      {
+        name: "Alcance 1",
+        data: [parseFloat(this.alcanceUno)]
+      },
+      {
+        name: "Alcance 2",
+        data: [parseFloat(this.alcanceDos)]
+      },
+      {
+        name: "Alcance 3",
+        data: [parseFloat(this.alcanceTres)]
+      }
+    ];
+    this.chartOptions.xaxis.categories = ["Porcentaje"];
+    this.chartOptions.title.text = "Porcentaje de Emisiones por Alcance";
+
+    this.chartOptions.tooltip.y.formatter = function(val: any) {
+      return val.toFixed(2) + "%";
+    };
+  }
 
   isAllSelected() {
     const numSelected = this.selection.selected.length;
@@ -313,7 +346,6 @@ export class ResultadosComponent implements OnInit {
     return str;
   }
 
-
   private descargarCSV(data: string, filename = 'download.csv'): void {
     const blob = new Blob(['\ufeff' + data], { type: 'text/csv;charset=utf-8;' });
     const dwldLink = document.createElement('a');
@@ -327,10 +359,9 @@ export class ResultadosComponent implements OnInit {
     dwldLink.click();
     document.body.removeChild(dwldLink);
   }
+
   public generarYDescargarCSV(): void {
     const csvData = this.convertToCSV(this.resumenEmisiones);
     this.descargarCSV(csvData, 'resumen-emisiones.csv');
   }
-
-
 }
